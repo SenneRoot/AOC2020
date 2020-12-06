bd534a0de15a25abdb836c6c5c1420ce0cfb5731
SenneRoot <senner@hotmail.nl>
2020-12-06 18:56:43 +0100

    cleanup day 6

--- day_6/task_2.py
+++ day_6/task_2.py
@@ -5,15 +5,15 @@ def count_answers(data):
     count = 0
     nPersons = len(data)
 
-    lAnswerMap = {}
+    answerMap = {}
     for pers in data:
-        for ans in pers.strip("\n"):
-            if ans not in lAnswerMap:
-                lAnswerMap[ans] = 1
+        for char in pers.strip("\n"):
+            if char not in answerMap:
+                answerMap[char] = 1
             else:
-                lAnswerMap[ans] += 1
+                answerMap[char] += 1
 
-    for val in lAnswerMap.values():
+    for val in answerMap.values():
         if val == nPersons:
             count += 1
 
@@ -21,11 +21,9 @@ def count_answers(data):
 
 
 if __name__ == "__main__":
-    inputs = read_input("input.txt")
-
     data = []
     count = 0
-    for input in inputs:
+    for input in read_input("input.txt"):
         if input == "\n":
             count += count_answers(data)
             data = []
