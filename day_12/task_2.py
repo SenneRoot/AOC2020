cfed595ac003dde896702299d5dd4263303d170a
SenneRoot <senner@hotmail.nl>
2020-12-12 14:39:19 +0100

    nasty but it works

--- day_12/task_2.py
+++ day_12/task_2.py
@@ -15,55 +15,47 @@ boatState = {"N" : 0,
              "W" : 0,
              "F" : "E"}
 
-wayPoint = {"N" : 4,
+wayPoint = {"N" : 1,
             "E" : 10,
             "S" : 0,
             "W" : 0,}
 
 direction = list(["N", "E", "S", "W"])
 
-# 1 90 deg rotation
-newWaypoint = wayPoint.copy()
-for key in wayPoint.keys():
-    newWaypoint[key] = wayPoint[direction[(direction.index(key) + turn["R"]) % len(direction)]]
-
-wayPoint = newWaypoint
-
-print("test")
 
 class Boat:
     def __init__(self, state, wayPoint):
-        self.state = state
-        self.wayPoint = wayPoint
+        self.state = state.copy()
+        self.wayPoint = wayPoint.copy()
 
     def performInstruction(self, instruction, value):
-        # turn
-        if instruction == "L" or instruction == "R":
-            boatState["F"] = direction[(direction.index(boatState["F"]) + int((turn[instruction] * (value/90)))) % len(direction)]
-        elif instruction == "F":
-            # add the value to the opposite direction were facing if this diection 
-            if value < self.state[opposite[self.state[instruction]]]:
-                self.state[opposite[self.state[instruction]]] -= value
-            else:
-                self.state[self.state[instruction]] += value - self.state[opposite[self.state[instruction]]]
-                self.state[opposite[self.state[instruction]]] = 0
-        else:
-            if value < self.state[opposite[instruction]]:
-                self.state[opposite[instruction]] -= value
-            else:
-                self.state[instruction] += (value - self.state[opposite[instruction]])
-                self.state[opposite[instruction]] = 0
+        if instruction == "F":
+            #perform step value times
+            for key in self.wayPoint.keys():
+                step = self.wayPoint[key] * value
+                # add the value to the opposite direction were facing if this diection 
+                if step < self.state[opposite[key]]:
+                    self.state[opposite[key]] -= step
+                else:
+                    self.state[key] += step - self.state[opposite[key]]
+                    self.state[opposite[key]] = 0
 
+           
     def moveWaypoint(self, instruction, value):
         # rotate: e += n, s += e, , w += s, n += w
         if instruction == "L" or instruction == "R":
             newWaypoint = self.wayPoint.copy()
-            for key in self.wayPoint.keys():
-                newWaypoint[key] = self.wayPoint[direction[(direction.index(key) + turn[instruction]) % len(direction)]]
-            self.wayPoint = newWaypoint.copy()
-
-            #boatState["F"] = direction[(direction.index(boatState["F"]) + int((turn[instruction] * (value/90)))) % len(direction)]
-
+            for i in range(0, int(value/90)):
+                for key in self.wayPoint.keys():
+                    newWaypoint[key] = self.wayPoint[direction[(direction.index(key) + turn[instruction]) % len(direction)]]
+                self.wayPoint = newWaypoint.copy()
+        else: #instruction = N, E, S, W
+            if value < self.wayPoint[opposite[instruction]]:
+                self.wayPoint[opposite[instruction]] -= value
+            else:
+                self.wayPoint[instruction] += (value - self.wayPoint[opposite[instruction]])
+                self.wayPoint[opposite[instruction]] = 0
+        
 
 if __name__ == "__main__":
     inputs = read_input("input.txt")
@@ -73,9 +65,12 @@ if __name__ == "__main__":
     for i, input in enumerate(inputs):
         instructions[i] = (input[:1], input[1:])
 
-    b = Boat(boatState)
+    b = Boat(boatState, wayPoint)
 
     for instruction in instructions.values():
-        b.performInstruction(instruction[0], int(instruction[1]))
+        if instruction[0] != "F":
+            b.moveWaypoint(instruction[0], int(instruction[1]))
+        else:
+            b.performInstruction(instruction[0], int(instruction[1]))
 
     print(abs(b.state["N"] - b.state["S"]) + abs(b.state["E"] - b.state["W"]))
