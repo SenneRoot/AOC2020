fd75829986a93ba64ff59f355dad3f2846769c7f
Senne Root <senne@hgg-group.com>
2020-12-12 13:53:30 +0100

    day 12 start

---
+++ day_12/task_2.py
@@ -0,0 +1,81 @@
+def read_input(filename):
+    return [str(x).rstrip() for x in open(filename)]
+
+turn = {"R" : -1,
+        "L" : 1}
+
+opposite = {"N" : "S",
+            "E" : "W",
+            "S" : "N",
+            "W" : "E"}
+
+boatState = {"N" : 0,
+             "E" : 0,
+             "S" : 0,
+             "W" : 0,
+             "F" : "E"}
+
+wayPoint = {"N" : 4,
+            "E" : 10,
+            "S" : 0,
+            "W" : 0,}
+
+direction = list(["N", "E", "S", "W"])
+
+# 1 90 deg rotation
+newWaypoint = wayPoint.copy()
+for key in wayPoint.keys():
+    newWaypoint[key] = wayPoint[direction[(direction.index(key) + turn["R"]) % len(direction)]]
+
+wayPoint = newWaypoint
+
+print("test")
+
+class Boat:
+    def __init__(self, state, wayPoint):
+        self.state = state
+        self.wayPoint = wayPoint
+
+    def performInstruction(self, instruction, value):
+        # turn
+        if instruction == "L" or instruction == "R":
+            boatState["F"] = direction[(direction.index(boatState["F"]) + int((turn[instruction] * (value/90)))) % len(direction)]
+        elif instruction == "F":
+            # add the value to the opposite direction were facing if this diection 
+            if value < self.state[opposite[self.state[instruction]]]:
+                self.state[opposite[self.state[instruction]]] -= value
+            else:
+                self.state[self.state[instruction]] += value - self.state[opposite[self.state[instruction]]]
+                self.state[opposite[self.state[instruction]]] = 0
+        else:
+            if value < self.state[opposite[instruction]]:
+                self.state[opposite[instruction]] -= value
+            else:
+                self.state[instruction] += (value - self.state[opposite[instruction]])
+                self.state[opposite[instruction]] = 0
+
+    def moveWaypoint(self, instruction, value):
+        # rotate: e += n, s += e, , w += s, n += w
+        if instruction == "L" or instruction == "R":
+            newWaypoint = self.wayPoint.copy()
+            for key in self.wayPoint.keys():
+                newWaypoint[key] = self.wayPoint[direction[(direction.index(key) + turn[instruction]) % len(direction)]]
+            self.wayPoint = newWaypoint.copy()
+
+            #boatState["F"] = direction[(direction.index(boatState["F"]) + int((turn[instruction] * (value/90)))) % len(direction)]
+
+
+if __name__ == "__main__":
+    inputs = read_input("input.txt")
+
+    instructions = dict()
+
+    for i, input in enumerate(inputs):
+        instructions[i] = (input[:1], input[1:])
+
+    b = Boat(boatState)
+
+    for instruction in instructions.values():
+        b.performInstruction(instruction[0], int(instruction[1]))
+
+    print(abs(b.state["N"] - b.state["S"]) + abs(b.state["E"] - b.state["W"]))
