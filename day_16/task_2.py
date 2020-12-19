348489a61facf90cdee69cbd150538557f6d4c37
SenneRoot <senner@hotmail.nl>
2020-12-19 12:23:47 +0100

    update task 2

--- day_16/task_2.py
+++ day_16/task_2.py
@@ -5,11 +5,10 @@ import math
 def findValues(yourTicket, nearByTickets, rules):
     possibleFields = {i: set(rules.keys()) for i in range(len(yourTicket))}
     for ticket in nearByTickets:
-        if not checkRules(ticket, rules): # returns list of size 0 if valid
-            for i, val in enumerate(ticket):
-                for k, ranges in rules.items():
-                    if val not in ranges[0] and val not in ranges[1]:
-                        possibleFields[i].remove(k)
+        for i, val in enumerate(ticket):
+            for k, ranges in rules.items():
+                if val not in ranges[0] and val not in ranges[1]:
+                    possibleFields[i].remove(k)
 
     result = [None for x in possibleFields]
 
@@ -21,10 +20,11 @@ def findValues(yourTicket, nearByTickets, rules):
     return math.prod([yourTicket[index] for index, dep in enumerate(result) if dep.startswith("departure")])
 
 if __name__ == "__main__":
-    rules, yourTicket, nearByTickets = formatInput(read_input("input.txt"))
-    
+    rules, yourTicket, nearByTickets = formatInput(read_input("test_input.txt"))
+    validTickets = []
+
     for nearByTicket in nearByTickets:
-       if checkRules(nearByTicket, rules):
-           nearByTickets.remove(nearByTicket)
+       if not checkRules(nearByTicket, rules):
+           validTickets.append(nearByTicket)
     
-    print(findValues(yourTicket, nearByTickets, rules))
+    print(findValues(yourTicket, validTickets, rules))
